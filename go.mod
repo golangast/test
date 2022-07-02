792974c7c542dd49ad2d8132e16cfc09f0a35bf6
zach <zendrulat@gmail.com>
2022-07-02 09:40:37 -0500

    validation and into jwt

--- go.mod
+++ go.mod
@@ -3,15 +3,19 @@ module test
 go 1.18
 
 require (
+	github.com/go-playground/validator v9.31.0+incompatible
+	github.com/golang-jwt/jwt v3.2.2+incompatible
 	github.com/labstack/echo/v4 v4.7.2
 	modernc.org/sqlite v1.17.3
 )
 
 require (
-	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
+	github.com/go-playground/locales v0.14.0 // indirect
+	github.com/go-playground/universal-translator v0.18.0 // indirect
 	github.com/google/uuid v1.3.0 // indirect
 	github.com/kballard/go-shellquote v0.0.0-20180428030007-95032a82bc51 // indirect
 	github.com/labstack/gommon v0.3.1 // indirect
+	github.com/leodido/go-urn v1.2.1 // indirect
 	github.com/mattn/go-colorable v0.1.11 // indirect
 	github.com/mattn/go-isatty v0.0.14 // indirect
 	github.com/remyoudompheng/bigfft v0.0.0-20200410134404-eec4a21b6bb0 // indirect
@@ -25,6 +29,7 @@ require (
 	golang.org/x/time v0.0.0-20201208040808-7e3f01d25324 // indirect
 	golang.org/x/tools v0.0.0-20201124115921-2c860bdd6e78 // indirect
 	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
+	gopkg.in/go-playground/assert.v1 v1.2.1 // indirect
 	lukechampine.com/uint128 v1.1.1 // indirect
 	modernc.org/cc/v3 v3.36.0 // indirect
 	modernc.org/ccgo/v3 v3.16.6 // indirect
--- main.go
+++ main.go
@@ -1,21 +1,110 @@
 package main
 
 import (
+	"crypto/subtle"
+	"fmt"
+	"github.com/go-playground/validator"
+	"github.com/golang-jwt/jwt"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
+	"net/http"
 	. "test/mtemplate"
 	. "test/router"
 )
 
+type (
+	User struct {
+		Name  string `json:"name" validate:"required" form:"name"`
+		Email string `json:"email" validate:"required,email" form:"email"`
+	}
+
+	CustomValidator struct {
+		validator *validator.Validate
+	}
+)
+
+func (cv *CustomValidator) Validate(i interface{}) error {
+	if err := cv.validator.Struct(i); err != nil {
+
+		for _, err := range err.(validator.ValidationErrors) {
+			fmt.Println(err.StructField())
+			fmt.Println(err.ActualTag())
+			fmt.Println(err.Kind())
+			fmt.Println(err.Value())
+			fmt.Println(err.Param())
+			fmt.Println("---------------")
+		}
+
+		// Optionally, you could return the error to give each route more control over the status code
+		return echo.NewHTTPError(http.StatusBadRequest, err.Error())
+	}
+	return nil
+}
 func main() {
 	e := echo.New()
-
+	e.Validator = &CustomValidator{validator: validator.New()}
 	e.Renderer = Rend()
+	g := e.Group("/admin")
 
 	Routes(e)
+	e.POST("/users", func(c echo.Context) (err error) {
+		u := new(User)
+		if err = c.Bind(u); err != nil {
+			return echo.NewHTTPError(http.StatusBadRequest, err.Error())
+		}
+		if err = c.Validate(u); err != nil {
+			return err
+		}
+		signingKey := []byte("secret")
+		// return c.JSON(http.StatusOK, u)
+		return c.Render(http.StatusOK, "home.html", map[string]interface{}{
+			"user": u,
+		})
+	})
+	e.Logger.Fatal(e.Start(":1323"))
+	g.Use(middleware.BasicAuth(func(username, password string, c echo.Context) (bool, error) {
+		// Be careful to use constant time comparison to prevent timing attacks
+		if subtle.ConstantTimeCompare([]byte(username), []byte("joe")) == 1 &&
+			subtle.ConstantTimeCompare([]byte(password), []byte("secret")) == 1 {
+			return true, nil
+
+		}
+		return false, nil
+	}))
+	g.GET("/here", Home)
+	e.Use(middleware.BasicAuthWithConfig(middleware.BasicAuthConfig{}))
+
+	config := middleware.JWTConfig{
+		TokenLookup: "query:token",
+		ParseTokenFunc: func(auth string, c echo.Context) (interface{}, error) {
+			keyFunc := func(t *jwt.Token) (interface{}, error) {
+				if t.Method.Alg() != "HS256" {
+					return nil, fmt.Errorf("unexpected jwt signing method=%v", t.Header["alg"])
+				}
+				return signingKey, nil
+			}
+
+			// claims are of type `jwt.MapClaims` when token is created with `jwt.Parse`
+			token, err := jwt.Parse(auth, keyFunc)
+			if err != nil {
+				return nil, err
+			}
+			if !token.Valid {
+				return nil, err
+			}
+			return token, nil
+		},
+	}
+
+	g.Use(middleware.JWTWithConfig(config))
+
 	e.Static("/static", "static")
 	e.Use(middleware.GzipWithConfig(middleware.GzipConfig{
 		Level: 5,
 	}))
 	e.Logger.Fatal(e.Start(":1323"))
+}
+func Home(c echo.Context) error {
+
+	return c.Render(http.StatusOK, "home.html", map[string]interface{}{})
 }
